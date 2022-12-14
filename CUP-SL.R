fd09b4e4e93341d85110a4e39065e14698725fc3
LucaMaro1 <lucamariniwork@gmail.com>
2022-12-14 22:18:18 +0100

    first implementation for cup dataset

---
+++ CUP-SL.R
@@ -0,0 +1,51 @@
+library(MLmetrics)
+library(SuperLearner)
+library(performanceEstimation)
+library(splitTools)
+
+#CUP
+cup_train = read.csv("./CUP/ML-CUP22-TR_noHeader.csv", header = FALSE)
+cup_train = cup_train[c(-1)]
+#controllo se esistono valori NA
+colSums(is.na(cup_train))
+# creo input - output train
+set.seed(1)
+split_id_cup <- partition(cup_train$V2, p = c(train = 0.7, valid = 0.3))
+train_input_cup = cup_train[split_id_cup$train, c(-10, -11)]
+train_output_1_cup = cup_train[split_id_cup$train, 10]
+train_output_2_cup = cup_train[split_id_cup$train, 11]
+validation_input_cup = cup_train[split_id_cup$valid, c(-10, -11)]
+validation_output_1_cup = cup_train[split_id_cup$valid,10]
+validation_output_2_cup = cup_train[split_id_cup$valid,11]
+#Superlearner
+set.seed(3)
+
+tune_ranger_cup = list(num.trees = c(500,1000,2000), mtry = c(floor(sqrt(ncol(train_input_cup))), ncol(train_input_cup)))
+learner_ranger_cup = create.Learner("SL.ranger", tune = tune_ranger_cup, detailed_names = TRUE, name_prefix = "ranger")
+
+tune_svm_rbf_cup = list(kernel = "rbfdot", sigma = c(0.06, 0.01, 0.1), C = c(0.8, 1, 1.2))
+learner_svm_rbf_cup = create.Learner("SL.ksvm", tune = tune_svm_rbf_cup, detailed_names = TRUE, name_prefix = "ksvm")
+
+#input1
+sl_cup_1 <- SuperLearner(Y = train_output_1_cup, X = train_input_cup, newX = validation_input_cup, family = gaussian(),
+                    SL.library = c(learner_ranger_cup$names ,learner_svm_rbf_cup$names),
+                    verbose = TRUE, cvControl = list(10, FALSE), control = list(TRUE, TRUE))
+
+sl_cup_1
+#input2
+sl_cup_2 <- SuperLearner(Y = train_output_2_cup, X = train_input_cup, newX = validation_input_cup, family = gaussian(),
+                         SL.library = c(learner_ranger_cup$names ,learner_svm_rbf_cup$names),
+                         verbose = TRUE, cvControl = list(10, FALSE), control = list(TRUE, TRUE))
+sl_cup_2
+# previsioni VALIDATION SET
+pred_cup_1 = predict.SuperLearner(object = sl_cup_1, newdata = validation_input_cup, onlySL = TRUE)
+pred_cup_2 = predict.SuperLearner(object = sl_cup_2, newdata = validation_input_cup, onlySL = TRUE)
+
+mae1 = MAE(pred_cup_1$pred, validation_output_1_cup)
+mae2 = MAE(pred_cup_2$pred, validation_output_2_cup)
+
+accuracy1 = 100 - (mae1 / mean(validation_output_1_cup))
+accuracy2 = 100 - (mae2 / abs(mean(validation_output_2_cup)))
+
+accuracy1
+accuracy2
--- monk-hot-SL.R
+++ monk-hot-SL.R
@@ -28,11 +28,14 @@ test_input_1 = subset(monk_test_1_hot,select = -V1)
 #setto seme per riprodurre la CV ogni volta
 #provare a vedere se si può cambiare iperparametri, es KSVM (vedi slide 9 SVM-other-info)
 set.seed(3)
+
 tune_ranger = list(num.trees = c(500,1000,2000),
                    mtry = c(floor(sqrt(ncol(train_input_1))),ncol(train_input_1)))
 learner_ranger = create.Learner("SL.ranger", tune = tune_ranger, detailed_names = TRUE, name_prefix = "ranger")
+
 tune_svm_rbf = list(kernel = "rbfdot", sigma = c(0.06, 0.01, 0.1), C = c(0.8, 1, 1.2))
 learner_svm_rbf = create.Learner("SL.ksvm", tune = tune_svm_rbf, detailed_names = TRUE, name_prefix = "ksvm")
+
 sl1 <- SuperLearner(Y = train_output_1, X = train_input_1, newX = validation_input_1, family = binomial(),
                          SL.library = c("SL.glm",learner_ranger$names ,learner_svm_rbf$names),
                          verbose = TRUE,cvControl=list(10,TRUE) ,control = list(TRUE, TRUE))
@@ -103,4 +106,3 @@ pred3 = predict(sl3, test_input_3, onlySL = TRUE)
 pred_rocr = ROCR::prediction(pred3$pred, test_output_3)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
-
