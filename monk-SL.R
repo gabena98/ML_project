75afd6e6056f49fb265f2ebde87bdb46eb3795e4
gabena98 <gabena98@gmail.com>
2022-12-21 13:37:01 +0100

    bug fix

--- monk-SL.R
+++ monk-SL.R
@@ -1,6 +1,5 @@
 library(corrplot)
 library(ggplot2)
-#library(mgcv)
 library(gam)
 library(SuperLearner)
 library(dplyr)                      # For the "filter" function
@@ -12,7 +11,7 @@ library(MLmetrics)
 
 #monk 1
 #cambiare il path per eseguire su altri computer
-monk_train_1=read.table("./Documents/MONK/monks-1.train", header = FALSE, sep = "", dec = ".")
+monk_train_1=read.table("./MONK/monks-1.train", header = FALSE, sep = "", dec = ".")
 #controllo se esistono valori NA
 colSums(is.na(monk_train_1))
 # creo input - output train
@@ -23,7 +22,7 @@ train_input_1 = monk_train_1[split_id$train,c(-1,-8)]
 validation_input_1 = monk_train_1[split_id$valid,c(-1,-8)]
 validation_output_1 = monk_train_1[split_id$valid,1]
 # creo input - output test
-monk_test_1 = read.table("./Documents/MONK/monks-1.test", header = FALSE, sep = "", dec = ".")
+monk_test_1 = read.table("./MONK/monks-1.test", header = FALSE, sep = "", dec = ".")
 colSums(is.na((monk_test_1)))
 test_output_1 = monk_test_1$V1
 test_input_1 = subset(monk_test_1,select = c(-V1,-V8))
@@ -36,11 +35,7 @@ tune_ranger = list(num.trees = c(500,1000,2000),
 learner_ranger = create.Learner("SL.ranger", tune = tune_ranger, detailed_names = TRUE, name_prefix = "ranger")
 tune_svm_rbf = list(kernel = "rbfdot", sigma = c(0.06, 0.01, 0.1), C = c(0.8, 1, 1.2))
 learner_svm_rbf = create.Learner("SL.ksvm", tune = tune_svm_rbf, detailed_names = TRUE, name_prefix = "ksvm")
-#tune_svm_poly = list(kernel = "polydot", degree = c(2, 2.5, 3), offset = 1, C = c(0.8, 1, 1.2))
-#tune_svm_poly = list(kernel = "polydot")
-#tune_svm_tanh = list(kernel = "tanhdot")
-#learner_svm_poly = create.Learner("SL.ksvm", tune = tune_svm_poly, detailed_names = TRUE, name_prefix = "ksvm")
-#learner_svm_tanh = create.Learner("SL.ksvm", tune = tune_svm_tanh, detailed_names = TRUE, name_prefix = "ksvm")
+
 sl1 <- SuperLearner(Y = train_output_1, X = train_input_1, newX = validation_input_1, family = binomial(),
                          SL.library = c("SL.glm",learner_ranger$names ,learner_svm_rbf$names),
                          verbose = TRUE,cvControl=list(10,TRUE) ,control = list(TRUE, TRUE))
@@ -55,7 +50,7 @@ pred1 = predict(sl1, test_input_1, onlySL = TRUE)
 pred_rocr = ROCR::prediction(pred1$pred, test_output_1)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
-
+{
 ### provare a fare risk assessment con CV.Superlearner, forse non serve
 # (num_cores = RhpcBLASctl::get_num_cores())
 # options(mc.cores = num_cores)
@@ -81,16 +76,16 @@ auc
 # #mostra qual è il miglior modello per ogni fold
 # table(simplify2array(cv_sl1$whichDiscreteSL))
 # print(review_weights(cv_sl1), digits = 3)
-
+}
 #monk 2
-monk_train_2=read.table("./Documents/MONK/monks-2.train",header = FALSE, sep = "", dec = ".")
+monk_train_2=read.table("./MONK/monks-2.train",header = FALSE, sep = "", dec = ".")
 set.seed(1)
 split_id <- partition(monk_train_2$V1, p = c(train = 0.7, valid = 0.3))
 train_output_2 = monk_train_2[split_id$train,1]
 train_input_2 = monk_train_2[split_id$train,c(-1,-8)]
 validation_input_2 = monk_train_2[split_id$valid,c(-1,-8)]
 validation_output_2 = monk_train_2[split_id$valid,1]
-monk_test_2 = read.table("./Documents/MONK/monks-2.test", header = FALSE, sep = "", dec = ".")
+monk_test_2 = read.table("./MONK/monks-2.test", header = FALSE, sep = "", dec = ".")
 test_output_2 = monk_test_2$V1
 test_input_2= subset(monk_test_2,select = c(-V1,-V8))
 set.seed(3)
@@ -110,14 +105,14 @@ auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.valu
 auc
 
 #monk3
-monk_train_3=read.table("./Documents/MONK/monks-3.train",header = FALSE, sep = "", dec = ".")
+monk_train_3=read.table("./MONK/monks-3.train",header = FALSE, sep = "", dec = ".")
 set.seed(1)
 split_id <- partition(monk_train_3$V1, p = c(train = 0.7, valid = 0.3))
 train_output_3 = monk_train_3[split_id$train,1]
 train_input_3 = monk_train_3[split_id$train,c(-1,-8)]
 validation_input_3 = monk_train_3[split_id$valid,c(-1,-8)]
 validation_output_3 = monk_train_3[split_id$valid,1]
-monk_test_3 = read.table("./Documents/MONK/monks-3.test", header = FALSE, sep = "", dec = ".")
+monk_test_3 = read.table("./MONK/monks-3.test", header = FALSE, sep = "", dec = ".")
 test_output_3 = monk_test_3$V1
 test_input_3 = subset(monk_test_3,select = c(-V1,-V8))
 set.seed(3)
@@ -135,19 +130,3 @@ pred3 = predict(sl3, test_input_3, onlySL = TRUE)
 pred_rocr = ROCR::prediction(pred3$pred, test_output_3)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
-
-#CUP dataset
-ml_cup_tr =read.table("./ML-22-PRJ lecture  package-20221108/ML-CUP22-TR.csv",header = FALSE, sep ="," , dec = ".")
-colSums(is.na((ml_cup_tr)))
-set.seed(10)
-split_id <- partition(ml_cup_tr$V12, p = c(train = 0.7, valid = 0.3))
-
-ml_cup_tr_output=ml_cup_tr[split_id$train,c(11,12)]
-ml_cup_tr_input=ml_cup_tr[split_id$train,2:10]
-
-ml_cup_ts_output=ml_cup_tr[split_id$valid,c(11,12)]
-ml_cup_ts_input=ml_cup_tr[split_id$valid,2:10]
-
-cup1<- SuperLearner(Y = ml_cup_tr_output$V11, X = ml_cup_tr_input, family = gaussian(),
-                    SL.library = c("SL.ksvm"))
-cup1
--- monk-hot-SL.R
+++ monk-hot-SL.R
@@ -10,20 +10,25 @@ library(MLmetrics)
 
 #monk 1
 monk_train_1_hot = read.csv("./MONK/monk_1_train_hot.csv", header = FALSE)
+monk_train_1_hot = monk_train_1_hot[,-1]
 #controllo se esistono valori NA
 colSums(is.na(monk_train_1_hot))
 # creo input - output train
 set.seed(1)
-split_id <- partition(monk_train_1_hot$V1, p = c(train = 0.7, valid = 0.3))
-train_output_1 = monk_train_1_hot[split_id$train,1]
+split_id <- partition(monk_train_1_hot$V2, p = c(train = 0.7, valid = 0.3))
 train_input_1 = monk_train_1_hot[split_id$train,-1]
+train_output_1_1 = monk_train_1_hot[split_id$train,1]
+#train_output_1_2 = monk_train_1_hot[split_id$train,2]
 validation_input_1 = monk_train_1_hot[split_id$valid,-1]
-validation_output_1 = monk_train_1_hot[split_id$valid,1]
+validation_output_1_1 = monk_train_1_hot[split_id$valid,1]
+#validation_output_1_2 = monk_train_1_hot[split_id$valid,2]
 # creo input - output test
 monk_test_1_hot = read.csv("./MONK/monk_1_test_hot.csv", header = FALSE)
+monk_test_1_hot = monk_test_1_hot[,-1]
 colSums(is.na((monk_test_1_hot)))
-test_output_1 = monk_test_1_hot$V1
-test_input_1 = subset(monk_test_1_hot,select = -V1)
+#test_output_1_1 = monk_test_1_hot$V1
+test_output_1_2 = monk_test_1_hot$V2
+test_input_1 = subset(monk_test_1_hot,select = -V2)
 #provo SuperLearner, se aggiungo SL.knn da errore durante le previsioni
 #setto seme per riprodurre la CV ogni volta
 #provare a vedere se si può cambiare iperparametri, es KSVM (vedi slide 9 SVM-other-info)
@@ -36,73 +41,126 @@ learner_ranger = create.Learner("SL.ranger", tune = tune_ranger, detailed_names 
 tune_svm_rbf = list(kernel = "rbfdot", sigma = c(0.06, 0.01, 0.1), C = c(0.8, 1, 1.2))
 learner_svm_rbf = create.Learner("SL.ksvm", tune = tune_svm_rbf, detailed_names = TRUE, name_prefix = "ksvm")
 
-sl1 <- SuperLearner(Y = train_output_1, X = train_input_1, newX = validation_input_1, family = binomial(),
+sl1 <- SuperLearner(Y = train_output_1_1, X = train_input_1, family = binomial(),
                          SL.library = c("SL.glm",learner_ranger$names ,learner_svm_rbf$names),
                          verbose = TRUE,cvControl=list(10,TRUE) ,control = list(TRUE, TRUE))
 sl1
 
-# previsioni VALIDATION SET
-pred_rocr = ROCR::prediction(sl1$SL.predict, validation_output_1)
-auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
-auc
-#previsioni TEST SET
-pred1 = predict(sl1, test_input_1, onlySL = TRUE)
-pred_rocr = ROCR::prediction(pred1$pred, test_output_1)
-auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
-auc
+# sl1_2 <- SuperLearner(Y = train_output_1_2, X = train_input_1, family = binomial(),
+#                     SL.library = c("SL.glm",learner_ranger$names ,learner_svm_rbf$names),
+#                     verbose = TRUE,cvControl=list(10,TRUE) ,control = list(TRUE, TRUE))
+# sl1_2
 
+# AUC TRAINING SET
+pred_rocr = ROCR::prediction(sl1$SL.predict, train_output_1_1)
+auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+auc
+# AUC VALIDATION SET
+pred1 = predict(sl1, validation_input_1, onlySL = TRUE)
+pred_rocr = ROCR::prediction(pred1$pred, validation_output_1_1)
+auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+auc
+# AUC TEST SET
+pred1 = predict(sl1, test_input_1, onlySL = TRUE)
+pred_rocr = ROCR::prediction(pred1$pred, test_output_1_2)
+auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+auc
 
 #monk 2
 monk_train_2_hot=read.csv("./MONK/monk_2_train_hot.csv",header = FALSE)
+monk_train_2_hot=monk_train_2_hot[,-1]
 colSums(is.na(monk_train_2_hot))
 set.seed(1)
-split_id <- partition(monk_train_2_hot$V1, p = c(train = 0.7, valid = 0.3))
-train_output_2 = monk_train_2_hot[split_id$train,1]
+split_id <- partition(monk_train_2_hot$V2, p = c(train = 0.7, valid = 0.3))
 train_input_2 = monk_train_2_hot[split_id$train,-1]
+train_output_2_1 = monk_train_2_hot[split_id$train,1]
+#train_output_2_2 = monk_train_2_hot[split_id$train,2]
 validation_input_2 = monk_train_2_hot[split_id$valid,-1]
-validation_output_2 = monk_train_2_hot[split_id$valid,1]
+validation_output_2_1 = monk_train_2_hot[split_id$valid,1]
+#validation_output_2_2 = monk_train_2_hot[split_id$valid,2]
 monk_test_2_hot = read.csv("./MONK/monk_2_test_hot.csv",header = FALSE)
-test_output_2 = monk_test_2_hot$V1
-test_input_2 = subset(monk_test_2_hot,select = -V1)
+monk_test_2_hot = monk_test_2_hot[,-1]
+#test_output_2_1 = monk_test_2_hot$V1
+test_output_2_2 = monk_test_2_hot$V2
+test_input_2 = subset(monk_test_2_hot,select = -V2)
 set.seed(1)
-sl2 <- SuperLearner(Y = train_output_2, X = train_input_2, newX = validation_input_2,family = binomial(),
+sl2 <- SuperLearner(Y = train_output_2_1, X = train_input_2,family = binomial(),
                     SL.library = c("SL.glm",learner_ranger$names,learner_svm_rbf$names),
                     verbose = TRUE, cvControl=list(10,TRUE),control = list(TRUE, TRUE))
 sl2
+# sl2_2 <- SuperLearner(Y = train_output_2_2, X = train_input_2,family = binomial(),
+#                     SL.library = c("SL.glm",learner_ranger$names,learner_svm_rbf$names),
+#                     verbose = TRUE, cvControl=list(10,TRUE),control = list(TRUE, TRUE))
+# sl2_2
 
-# previsioni VALIDATION SET
-pred_rocr = ROCR::prediction(sl2$SL.predict, validation_output_2)
+# AUC TRAINING SET
+pred_rocr = ROCR::prediction(sl2$SL.predict, train_output_2_1)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
-# previsioni TEST SET
+# AUC VALIDATION SET
+pred2 = predict(sl2, validation_input_2, onlySL = TRUE)
+pred_rocr = ROCR::prediction(pred2$pred, validation_output_2_1)
+auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+auc
+# AUC TEST SET
 pred2 = predict(sl2, test_input_2, onlySL = TRUE)
-pred_rocr = ROCR::prediction(pred2$pred, test_output_2)
+pred_rocr = ROCR::prediction(pred2$pred, test_output_2_2)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
-
+{
+# # AUC TRAINING SET
+# pred_rocr = ROCR::prediction(sl2_2$SL.predict, train_output_2_2)
+# auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+# auc
+# # AUC VALIDATION SET
+# pred2 = predict(sl2_2, validation_input_2, onlySL = TRUE)
+# pred_rocr = ROCR::prediction(pred2$pred, validation_output_2_2)
+# auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+# auc
+# # AUC TEST SET
+# pred2 = predict(sl2_2, test_input_2, onlySL = TRUE)
+# pred_rocr = ROCR::prediction(pred2$pred, test_output_2_2)
+# auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+# auc
+}
 #monk3
 monk_train_3_hot=read.csv("./MONK/monk_3_train_hot.csv",header = FALSE)
+monk_train_3_hot = monk_train_3_hot[,-1]
 set.seed(1)
-split_id <- partition(monk_train_3_hot$V1, p = c(train = 0.7, valid = 0.3))
-train_output_3 = monk_train_3_hot[split_id$train,1]
+split_id <- partition(monk_train_3_hot$V2, p = c(train = 0.7, valid = 0.3))
 train_input_3 = monk_train_3_hot[split_id$train,-1]
+train_output_3_1 = monk_train_3_hot[split_id$train,1]
+#train_output_3_2 = monk_train_3_hot[split_id$train,2]
 validation_input_3 = monk_train_3_hot[split_id$valid,-1]
-validation_output_3 = monk_train_3_hot[split_id$valid,1]
+validation_output_3_1 = monk_train_3_hot[split_id$valid,1]
+#validation_output_3_2 = monk_train_3_hot[split_id$valid,2]
 monk_test_3_hot = read.csv("./MONK/monk_3_test_hot.csv",header = FALSE)
-test_output_3 = monk_test_3_hot$V1
-test_input_3 = subset(monk_test_3_hot,select = -V1)
+monk_test_3_hot = monk_test_3_hot[,-1]
+#test_output_3_1 = monk_test_3_hot$V1
+test_output_3_2 = monk_test_3_hot$V2
+test_input_3 = subset(monk_test_3_hot,select = -V2)
 set.seed(3)
-sl3 <- SuperLearner(Y = train_output_3, X = train_input_3, newX = validation_input_3,family = binomial(),
+sl3 <- SuperLearner(Y = train_output_3_1, X = train_input_3,family = binomial(),
                     SL.library = c("SL.glm",learner_ranger$names,learner_svm_rbf$names),
                     verbose = TRUE, cvControl=list(10,TRUE),control = list(TRUE, TRUE))
 sl3
+# sl3_2 <- SuperLearner(Y = train_output_3_2, X = train_input_3,family = binomial(),
+#                     SL.library = c("SL.glm",learner_ranger$names,learner_svm_rbf$names),
+#                     verbose = TRUE, cvControl=list(10,TRUE),control = list(TRUE, TRUE))
+# sl3_2
 
-#previsioni VALIDATION SET
-pred_rocr = ROCR::prediction(sl3$SL.predict, validation_output_3)
+#AUC TRAINING SET
+pred_rocr = ROCR::prediction(sl3$SL.predict, train_output_3_1)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
-#previsioni TEST SET
+#AUC TEST SET
+pred3 = predict(sl3, validation_input_3, onlySL = TRUE)
+pred_rocr = ROCR::prediction(pred3$pred, validation_output_3_1)
+auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
+auc
+#AUC TEST SET
 pred3 = predict(sl3, test_input_3, onlySL = TRUE)
-pred_rocr = ROCR::prediction(pred3$pred, test_output_3)
+pred_rocr = ROCR::prediction(pred3$pred, test_output_3_2)
 auc = ROCR::performance(pred_rocr, measure = "auc", x.measure = "cutoff")@y.values[[1]]
 auc
+
